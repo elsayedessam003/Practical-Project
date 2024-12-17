5e7c74d8b491260c0146014fb7dbb44a87a398d4
AmrMedhat360 <115822550+AmrMedhat360@users.noreply.github.com>
2024-12-17 13:47:29 -0800

    Signup

--- Results/Results.js
+++
@@ -1,9 +0,0 @@
-let result=document.querySelector('.result');
-const getScore = localStorage.getItem("Score");
-const getQuestionNumber = localStorage.getItem("QuestionNumber");
-
-if(getScore==getQuestionNumber){
-    result.textContent="Congratulations! You have got the full-mark";
-}else{
-    result.textContent=`${getScore}/${getQuestionNumber}`;
-}
